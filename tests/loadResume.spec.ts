e890a6070e0e1e5b24cdf5cf740da30e615f6ebc
Will Migdol <will.migdol@gmail.com>
2025-02-10 12:52:57 -0800

    remove one test

--- tests/loadResume.spec.ts
+++
@@ -1,7 +0,0 @@
-import {test, expect, Page} from '@playwright/test';
-import loadResume from './utils/loadResume';
-import forEachResume from './utils/forEachResume';
-
-test('Ensure loading the resume works', async ({page}) => {
-    forEachResume(page, ()=>{});
-})
