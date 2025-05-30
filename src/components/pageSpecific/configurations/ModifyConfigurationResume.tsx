5f3be90e6d301aced559a9883991cb60a17de690
Will Migdol <will.migdol@gmail.com>
2025-05-29 23:18:17 -0700

    add tabs to configurations

--- src/components/pageSpecific/configurations/ModifyConfigurationResume.tsx
+++ src/components/pageSpecific/configurations/ModifyConfigurationResume.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from "react";
+import { Fragment, useContext, useEffect, useRef } from "react";
 import ConfigurationsModalsContext from "./ConfigurationsModalsContext";
 import LoadingSpinner from "@/components/LoadingSpinner";
 
@@ -7,17 +7,29 @@ function ConfigurationPane(){
     console.log(configuration);
     if(!configuration)
         return;
+
+    type Tab = {
+        name: string;
+        content: React.ReactNode;
+    }
+
+    const tabs: Tab[] = [
+        {
+            name: 'Basics',
+            content: <>
+                <h2 className="mt-0">Edit Basics</h2>
+            </>
+        }
+    ]
     return <>
         <h2 className="mt-0">{configuration.name}</h2>
         <div className="tabs tabs-box">
-            <input type="radio" name="my_tabs_6" className="tab" aria-label="Tab 1" />
-            <div className="tab-content bg-base-100 border-base-300 p-6">Tab content 1</div>
-
-            <input type="radio" name="my_tabs_6" className="tab" aria-label="Tab 2" defaultChecked />
-            <div className="tab-content bg-base-100 border-base-300 p-6">Tab content 2</div>
-
-            <input type="radio" name="my_tabs_6" className="tab" aria-label="Tab 3" />
-            <div className="tab-content bg-base-100 border-base-300 p-6">Tab content 3</div>
+            {tabs.map(tab => {
+                return <Fragment key={tab.name}>
+                    <input type="radio" name="configure-resume-tabs" className="tab" aria-label={tab.name} defaultChecked/>
+                    <div className="tab-content bg-base-100 border-base-300 p-6">{tab.content}</div>
+                </Fragment>
+            })}
         </div>
     </>
 }
