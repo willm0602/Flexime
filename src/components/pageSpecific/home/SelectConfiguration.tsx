ba1d8c36ffbbfe29f46aecb38675a5f07698283b
Will Migdol <will.migdol@gmail.com>
2025-05-31 18:06:25 -0700

    wording fix

---
+++ src/components/pageSpecific/home/SelectConfiguration.tsx
@@ -0,0 +1,32 @@
+import type { Configuration } from "@/lib/types/configuration";
+import { useContext, useEffect, useState } from "react";
+import ResumeContext from "./ResumeContext";
+import type { User } from "@supabase/supabase-js";
+import { getSavedConfigurationsForUser } from "@/lib/configurations";
+import { get } from "sortablejs";
+
+export default function SelectConfiguration({user}: {user: User | null}) {
+    
+    const [configurations, setConfigurations] = useState<Configuration[]>([]);
+    const {setResume} = useContext(ResumeContext);
+    
+    useEffect(() => {
+        getSavedConfigurationsForUser(user).then((configurations) => {
+            setConfigurations(configurations);
+        })
+    }, [user]);
+
+    getSavedConfigurationsForUser(user).then((configurations) => {
+        console.log(configurations);
+    });
+
+    if((configurations?.length || 0) === 0)
+        return null;
+
+    return <select id="load-resume-config" onChange={(e) => setResume(configurations[+e.target.value].resume)} className="select select-bordered w-full max-w-xs my-4" defaultValue={-1}>
+        <option disabled value={-1}>Load Configuration</option>
+        {configurations.map((configuration, idx) => {
+            return <option key={configuration.id} value={idx}>{configuration.name}</option>
+        })}
+    </select>
+}
