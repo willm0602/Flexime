011bef8425c4613287ede71a27583e93c91dca77
Will Migdol <will.migdol@gmail.com>
2025-07-07 11:04:09 -0700

    build fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
     plugins: {
-        '@tailwindcss/postcss': {},
+      '@tailwindcss/postcss': {},
+      autoprefixer: {},
     },
 };
-
-export default config;
